d324f6c8a99740f46e4cb4a1957f804fa79d42d8
Egor <lea@srsekb.ru>
2017-08-09 20:35:32 +0500

    Домашнее задание к лекции 2.1 «Создаем клиенты и сервера используя модуль http»

---
+++ HttpService.js
@@ -0,0 +1,65 @@
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+const https = require('https');
+const url = 'http://netology.tomilomark.ru/api/v1/hash';
+function handler(req, res) {
+    let data = '';
+    req.on('data', chunk => data += chunk);
+    req.on('end', () => {
+        data = JSON.parse(data);
+        //console.log(data);
+        getHash(data)
+        .then(outData =>{
+            res.writeHead(200,'OK', {'Content-Type': 'application/json'});
+            res.write(outData);
+            res.end();
+        })
+    });
+}
+
+// возвращает промис, который разрешается Json c секретным ключом
+function getHash(data) {
+    return new Promise((resolve, reject) => {
+        let param = {'lastName': data.lastName}.toString();
+        let options = {
+            hostname: url,
+            //port: 80,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(param),
+                'firstName': data.firstName
+            }
+        };
+
+        const request = https.request(options);
+        request.write(param);
+        request.end();
+        request.on('error',err => console.error(err));
+
+        request.on('response', ()=>{
+            let hashData = '';
+            response.on('data', function (chunk) {hashData += chunk});
+            response.on('end', ()=>{
+                data.hash = JSON.parse(hashData).hash;
+                resolve(data);
+            });
+        });
+    })
+}
+
+
+const server = http.createServer();
+server.on('error', err => console.error(err));
+server.on('request', handler);
+server.on('listening', () => {
+    console.log('Start HTTP on port %d', PORT);
+});
+server.listen(PORT);
+
+
+
+
+
+
+
