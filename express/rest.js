d99b658de425610bb4700615edee499973a5c7c4
Egor <lea@srsekb.ru>
2017-08-16 13:14:48 +0500

    Домашнее задание к лекции 2.2 «Создаем простое API, используя express» 2

--- express/rest.js
+++ express/rest.js
@@ -20,45 +20,42 @@ restAPIv1.get("/users/", function(req, res) {
 });
 
 restAPIv1.post("/users/", function(req, res) {
-    try {
-        let name = req.query.name;
-        let score = req.query.score;
-    }
-    catch (err) {res.status(400).send('Bad request!');};
+    let name = req.query.name;
+    let score = req.query.score;
+    if (name===undefined||score===undefined) {res.status(400).send('Bad request!');};
+
     let newUser = new User(name,score);
     userID = usersID.push(newUser);
-    newUser.ID = userID;
+    newUser.id = userID;
     users.push(newUser);
-    res.status(200).end(userID);
+    res.status(200).end(JSON.stringify(userID));
 });
 
 restAPIv1.get("/users/:id", function(req, res) {
-    try {
-        let ID = req.ID;
-    }
-    catch (err) {res.status(400).send('Bad request!')};
-    res.status(200).end(JSON.stringify(users.find(user => user.ID == req.ID )));
+    let id = req.params.id;
+    if (id===undefined) {res.status(400).send('Bad request!');};
+    let user = users.find(user => user.id == req.params.id )
+    if (user === undefined) res.status(400).send('Wrong user\'s ID!');
+    res.status(200).end(JSON.stringify(user));
 });
 
 restAPIv1.put("/users/:id", function(req, res) {
-    try {
-        let ID = req.ID;
-        let score = req.score;
-    }
-    catch (err) {res.status(400).send('Bad request!')};
-    let user = users.find(user => user.ID == req.ID );
+    let id = req.params.id;
+    let score = req.query.score;
+    if (id===undefined||score===undefined) {res.status(400).send('Bad request!')};
+
+    let user = users.find(user => user.id == req.params.id);
     if (user === undefined) res.status(400).send('Wrong user\'s ID!');
-    user.score = req.score;
+    user.score = req.query.score;
     res.status(200).send('user\'s score is updated!');
 });
 
 restAPIv1.delete("/users/:id", function(req, res) {
-    try {
-        let ID = req.ID;
-    }
-    catch (err) {res.status(400).send('Bad request!')};
-    let userIDX = users.findIndex(user => user.ID == req.ID );
+    let id = req.params.id;
+    if (id === undefined) {res.status(400).send('Bad request!')};
+    let userIDX = users.findIndex(user => user.id == id );
     if (userIDX === undefined) res.status(400).send('Wrong user\'s ID!');
+    console.log(userIDX);
     users.splice(userIDX,1);
     res.status(200).send('User deleted!');
 });
